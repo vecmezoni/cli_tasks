700ae4a28a12ff45a24c944433f816b37bc5b80a
Alexander Inozemtsev <alexander.inozemtsev@gmail.com>
2013-01-20 00:25:09 +0400

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,5 +2,5 @@
 
 sed -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/^Part \(.*\)$/<h2>Part \1<\/h2>/' -e '/<\/h2>$/! s/$/<br\/>/' -e '1 s/^/<html><body>\
 /g' -e '$ s/$/\
-<\/body><\/html>/' 2.txt
+<\/body><\/html>/' $1
 
