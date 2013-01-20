b4586d53d6c2384045d0b7dc0d3abb66e8ca2391
Inozemtsev Alexander <a.inozemtsev@me.com>
2013-01-20 22:50:47 +0400

    Problems 1 and 2 refactored

--- 2.sh
+++ 2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-sed -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/^Part \(.*\)$/<h2>Part \1<\/h2>/' -e '/<\/h2>$/! s/$/<br\/>/' -e '1 s/^/<html><body>\
-/g' -e '$ s/$/\
-<\/body><\/html>/' 2.txt
+echo '<html><body>'
+sed 's/&/\&amp;/g' $1 |
+sed 's/</\&lt;/g' |
+sed 's/>/\&gt;/g' |
+sed 's/\"/\&quot;/g' |
+sed 's/^Part \(.*\)$/<h2>Part \1<\/h2>/' |
+sed '/<\/h2>$/! s/$/<br\/>/' 
+echo '</body></html>'
 
